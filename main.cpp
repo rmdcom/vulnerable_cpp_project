96f4c00fc500bfc0c5f9ceb74dcab46920e2d5ea
rmdcom <rmdcom@ac.com>
2025-10-30 23:53:12 +0100

    intro CVES issues into repo

--- main.cpp
+++ main.cpp
@@ -1,17 +1,7 @@
+
 #include <libxml/parser.h>
 #include <sqlite3.h>
 #include <expat.h>
-#include <iostream>
-#include <string>
-#include <cstring>  // Add this line for strlen()
-#include <iostream>
-#include <string>
-#include <cstring>
-#include <expat.h>
-#include <curl/curl.h>
-#include <openssl/ssl.h>
-#include <openssl/crypto.h>
-#include <zlib.h>
 
 void test_libxml2() {
     xmlDocPtr doc = xmlReadMemory("<root><child/></root>", 22, "noname.xml", NULL, 0);
@@ -30,21 +20,14 @@ void test_expat() {
     XML_ParserFree(parser);
 }
 
+#include <iostream>
+#include <curl/curl.h>
+#include <openssl/ssl.h>
+#include <zlib.h>
+
 int main() {
     std::cout << "Testing vulnerable dependencies..." << std::endl;
-    
-    // For OpenSSL version >= 3.0
-    #if OPENSSL_VERSION_NUMBER >= 0x30000000L
-        std::cout << "OpenSSL version: " << OpenSSL_version_num() << std::endl;
-    #else
-        std::cout << "OpenSSL version: " << SSLeay_version(SSLEAY_VERSION) << std::endl;
-    #endif
-    
-    std::cout << "zlib version: " << zlibVersion() << std::endl;
-
     curl_version_info_data* curl_info = curl_version_info(CURLVERSION_NOW);
-    std::cout << "CURL version: " << curl_info->version << std::endl;
-
     std::cout << "libcurl version: " << curl_info->version << std::endl;
     std::cout << "OpenSSL version: " << OpenSSL_version(OPENSSL_VERSION) << std::endl;
     std::cout << "zlib version: " << zlibVersion() << std::endl;
